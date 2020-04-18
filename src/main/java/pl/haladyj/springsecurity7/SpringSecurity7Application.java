cdf86168f8a52f6191527f800f79fcce54cfb0bf
Pawel Haladyj <pawel@haladyj.pl>
2020-04-18 08:11:31 +0200

    initial commit

---
+++ src/main/java/pl/haladyj/springsecurity7/SpringSecurity7Application.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity7Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/haladyj/springsecurity7/SpringSecurity7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.springsecurity7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
